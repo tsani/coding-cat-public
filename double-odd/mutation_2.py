768f6ef17852fb0a3d6ab6bf19f597a1a97aaaf1
Sebastian42-5 <sebassotosandrea@gmail.com>
2024-11-27 12:53:41 -0500

    setting up the mutations

--- double-odd/mutation_2.py
+++ double-odd/mutation_2.py
@@ -1,2 +1,10 @@
 def double_odd(i_list):
+    '''''
+    mutation 2
+    '''
 
+    new_list = []
+    for i in range(1, len(i_list), 2):
+        if i_list[i] % 2 != 0:
+            new_list.append(i_list[i])
+    return new_list
