101ed7b9814181bcbab92ac885543bf611b2f7a7
Diana <andrei-diana@outlook.com>
2024-11-27 14:12:19 -0500

    make 5 mutations for zigzag

---
+++ mutations-zigzag/1mutation.py
@@ -0,0 +1,10 @@
+def zig_zag(s):
+    s = list(s)  # Convert string to list for pop operations
+    result = ""
+    for i in range(len(s)):
+        if i % 2 == 0:  # Even index
+            result += s.pop(0)  # Remove from the beginning
+        else:  # Odd index
+            result += s.pop(-1)  # Remove from the end
+    """ Missing return statement here""""
+
