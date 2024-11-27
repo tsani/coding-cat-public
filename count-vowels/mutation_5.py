b45603eb7b1550291c8f446d134d4d6cd876c225
William Poirier <poirierwilliam16@gmail.com>
2024-11-27 15:02:22 -0500

    changed mutation5

--- count-vowels/mutation_5.py
+++ count-vowels/mutation_5.py
@@ -1,6 +1,7 @@
 def count_vowels(string):
     lst = []
     for i in string:
-        if i in ['aeiouyAEIOUY']:
-            lst.pop(i) #pop instead of append
-    return len(lst)
+        if i in ['a','i','e','o','u','y','A','I','E','O','U','Y']:
+            lst.append(i) #pop instead of append
+    len(lst)
+count_vowels('a')
