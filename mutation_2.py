9d7bf21d6415d7eaf2d5ca8d64394a4a2f4701f8
AlemoExe <alecmorin99@gmail.com>
2024-11-27 13:33:46 -0500

    Update mutation_2.py

--- mutation_2.py
+++ mutation_2.py
@@ -4,7 +4,7 @@ def range_chars(a: str, b: str) -> str:
   This will unnecessarily reduce  the range of iterations skipping valid some comparisons
   """
   result = ""
-    n = min(len(a), len(b))  # Handle unequal lengths
+    n = min(len(a), len(b))
     for i in range(n):
         if (i > 0 and a[i] == b[i-1]) or a[i] == b[i] or (i < n - 1 and a[i] == b[i+1]):
             result += a[i]
