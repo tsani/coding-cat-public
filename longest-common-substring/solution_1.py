eb8c0a000640a9cee6310e4d09e556b9015672c3
unknown <nicoleslanina12@gmail.com>
2024-11-29 10:28:22 -0500

    2 solutions added

---
+++ longest-common-substring/solution_1.py
@@ -0,0 +1,10 @@
+def longest_common_substring(a, b):
+    max_len = 0
+    for i in range(len(a)):
+        for j in range(i + 1, len(a) + 1):
+            substring = a[i:j]
+            if substring in b:
+                max_len = max(max_len, j - i) 
+                
+    return max_len
+
