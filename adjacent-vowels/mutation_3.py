b954548dacc5efa0be0d2c33040642ce56f04c99
loicn <l@l>
2024-11-30 15:51:30 -0500

    Adding New mutations on partner's problem and modifying test cases

---
+++ adjacent-vowels/mutation_3.py
@@ -0,0 +1,11 @@
+def adjacent_vowels(word: str) -> int:
+    """
+    Wrong length check
+    """
+    vowels = "aeiouAEIOU"
+    if len(word) < 3:
+        return 1
+    if word[0] in vowels and word[1] in vowels:
+        return 1 + adjacent_vowels(word[1:])
+
+    return adjacent_vowels(word[1:])
