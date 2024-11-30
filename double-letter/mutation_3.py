419bb470c56541b3dad501cb71ebf59ccf107d96
Narshael <Narsh2007@gmail.com>
2024-11-30 16:01:04 -0500

    mutation_2 and mutation_3

--- double-letter/mutation_3.py
+++ double-letter/mutation_3.py
@@ -1,16 +1,16 @@
 def double_letter(word):
     '''
-        Limiting the iteration to len(result)-2, 
-        the loop will ignore the last two characters of the string
+        Change count condition,
+        looking at the wrong consonant
     '''
     consonants = "bcdfghjklpmntrqsvwxyzBCDFGHJKLMNPQRSTVWXYZ"  
     consonant_count = 0 
     result = list(word) 
 
-    for i in range(len(result)-2):
+    for i in range(len(result)):
         if result[i] in consonants:
             consonant_count += 1
-            if consonant_count == 2: 
+            if consonant_count == 3: # Mutation looking for the third consonant instead of the second consonant
                 result[i] = result[i] * 2 
                 break 
     return "".join(result) 
