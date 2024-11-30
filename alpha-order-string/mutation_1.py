3302722a13b7058a96624c130c5e8a338d12a388
user <user@users-MacBook-Pro.local>
2024-11-30 18:18:05 -0500

    mut 1

---
+++ alpha-order-string/mutation_1.py
@@ -0,0 +1,14 @@
+def alpha_order(word : str) -> str:
+	'''
+	Swap the values after second for loop
+	'''
+	new_word = ""
+        word = list(word)
+        for n in range(len(word)-1):
+                for n in range(len(word)-1):
+                        if word[n] > word[n+1]:
+				word[n], word[n+1] = word[n], word[n+1]
+        for n in range(len(word)):
+                new_word += word[n]
+        return new_word
+				
