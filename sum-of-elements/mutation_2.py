dc5a1f4af33781823af12ec6cbb03068e785f1ec
Jo-9m-n1 <leejongmin1092@gmail.com>
2024-11-29 19:49:53 -0500

    Update mutation_2.py
    Changed some errors

--- sum-of-elements/mutation_2.py
+++ sum-of-elements/mutation_2.py
@@ -1,11 +1,11 @@
 def unique_sum(lst):
-  unique_elements = set()
-  duplicates = set()
-  for num in lst:
-    if num in unique_elements.remove(num):
-      unique_elements.remove(num)
-      duplicates.add(num)
-    elif num not in duplicates:
-      unqiue_elements.add(num)
+    unique_elements = set()
+    duplicates = set()
+    for num in lst:
+        if num in unique_elements.remove(num):
+            unique_elements.remove(num)
+            duplicates.add(num)
+        elif num not in duplicates:
+            unqiue_elements.add(num)
     return(unique_elements)
     """It won't work since it will just return the list of unique elements and not the sum"""
