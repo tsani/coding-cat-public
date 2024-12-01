ddc23b6651f4839d0ffa1a0e64a45473c828d4f9
doykaberry <jdc9229@gmail.com>
2024-12-01 15:26:35 -0500

    mutation 1

---
+++ Neighbouring_Repeats/mutation_1.py
@@ -0,0 +1,20 @@
+def neighbour_repeat(word: str) -> str:
+    '''
+    Forgot a return statement
+    '''
+    new_word = ""
+    if len(word) == 1:
+        return word
+    elif len(word) == 2:
+        if word[0] == word[1]:
+            return new_word
+        return word
+    for i in range(1, len(word)-1):
+        if word[i] != word[i-1] and word[i] != word[i+1]:
+            new_word += word[i]
+    if len(word) > 2:
+        if word[-1] != word[-2]:
+            new_word += word[-1]
+        if word[0] != word[1]:
+            return word[0] + new_word
+    # Missing a return statement here
