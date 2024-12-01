31cf1de2e64f79ae335cbcc0d8b8ed5557740dd6
Zaccaria Broomhall Bedont <zaccariamontreal@gmail.com>
2024-12-01 14:03:04 -0500

    changed mutation 3

--- consecutive-count/mutation_3.py
+++ consecutive-count/mutation_3.py
@@ -1,9 +1,9 @@
 def consecutive_count(nums):
     '''
-    comparing pos n-1 to n+1
+    change == to !=
     '''
     count = 0
     for n in range(len(nums)-1):
-        if nums[n-1] == nums[n+1]:
+        if nums[n] != nums[n+1]:
             count += 1
     return count
