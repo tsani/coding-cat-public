39325c3191096a575649bc0bf133c661c08fbaab
Tariq-Menla <tariqmenla@gmail.com>
2024-12-01 15:24:18 -0500

    Remutated to the ideal code

--- count-peaks/mutation_4.py
+++ count-peaks/mutation_4.py
@@ -3,12 +3,7 @@ def count_peaks(nums):
         Forgot to return the statement.
     """
     count = 0
-    for num in enumerate(nums):
-        if num[0] == 0 or num[0] == len(nums) - 1:
-            continue
-        prev = nums[num[0] - 1]
-        next = nums[num[0] + 1]
-
-        if prev < num[1] and num[1] > next:
+    for i in range(1, len(nums) - 1):
+        if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
             count += 1
-# Missing in this line
+# Missing a return statement in this line
