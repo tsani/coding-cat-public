24e3a1c755186fe62b0c1c0f6b7678a92f209aab
RayanKhadhri <rayan.khadhri@dawsoncollege.qc.ca>
2024-12-01 16:27:31 -0500

    Delete range-chars/Range Characters directory

--- range-chars/Range Characters/solution.py
+++
@@ -1,6 +0,0 @@
-def range_chars(a: str, b:str) -> str:
-    result = ""
-    for i in range(len(a)):
-        if (b[i] == a[i]) or (i > 0 and b[i - 1] == a[i]) or (i < len(a) - 1 and b[i + 1] == a[i]):
-            result += a[i] 
-    return result
