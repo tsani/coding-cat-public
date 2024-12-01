e8a579302fe6053ed415b028de1a937ddf48a75a
doykaberry <jdc9229@gmail.com>
2024-12-01 15:28:12 -0500

    Mutation 2

---
+++ Neighbouring_Repeats/mutation_2.py
@@ -0,0 +1,20 @@
+def neighbour_repeat(word: str) -> str:
+    '''
+    Forgot to stricten the loop boundary
+    '''
+    new_word = ""
+    if len(word) == 1:
+        return word
+    elif len(word) == 2:
+        if word[0] == word[1]:
+            return new_word
+        return word
+    for i in range(len(word)-1): # The indexing should start at 1
+        if word[i] != word[i-1] and word[i] != word[i+1]:
+            new_word += word[i]
+    if len(word) > 2:
+        if word[-1] != word[-2]:
+            new_word += word[-1]
+        if word[0] != word[1]:
+            return word[0] + new_word
+    return new_word
