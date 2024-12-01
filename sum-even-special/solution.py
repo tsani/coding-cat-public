5934c153034dd6826bdba3b18a327583125788e8
Leo <leokh0713@gmail.com>
2024-12-01 09:28:06 -0500

    adding solution to sum-even-special/

---
+++ sum-even-special/solution.py
@@ -0,0 +1,13 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range (2,n):
+        if n % i == 0:
+            return False
+    return True
+def sum_even_special(lst):
+    total_sum = 0
+    for num in lst :
+        if num % 2 == 0 and (is_prime(num) or (num%3) == 0):
+            total_sum += num
+    return total_sum
