ac0d6c56668042db36811112bdc117b027b59dae
Sandor-B <sandor.balogh@dawsoncollege.qc.ca>
2024-11-30 22:24:03 -0500

    Add files via upload
    Assignment #4 submission: mutations for problem "count-repeat"

--- count-repeat/mutation_2.py
+++ count-repeat/mutation_2.py
@@ -2,17 +2,19 @@ def countRepeat(a, b):
     '''
         No return statement
     '''
-    count = 0 # Variable used to store if the previous element was b 
-    previous_is_b = False # Variable used to prevent overcounting a series of repetitions
+    count = 0
+    previous_is_b = False 
     repeat_lock = False
     
     for i in a:  
         if i == b:  
             if previous_is_b == True and repeat_lock == False:  
-                count += 1 # Once a repetition is counted, we have to prevent this from running in order to avoid overcounting
+                count += 1 
                 repeat_lock = True  
             previous_is_b = True  
         else:  
             repeat_lock = False
             previous_is_b = False
+
+    #Missing return statement here
             
