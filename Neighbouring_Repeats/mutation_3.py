c2a177a342090d80bdc53553ef11eb87ef46202a
doykaberry <jdc9229@gmail.com>
2024-12-01 15:28:34 -0500

    Mutation 3

---
+++ Neighbouring_Repeats/mutation_3.py
@@ -0,0 +1,16 @@
+def neighbour_repeat(word: str) -> str:
+    '''
+    Forgot to add a check for words of length 2
+    '''
+    new_word = ""
+    if len(word) == 1:
+        return word
+    for i in range(1, len(word)-1):
+        if word[i] != word[i-1] and word[i] != word[i+1]:
+            new_word += word[i]
+    if len(word) > 2:
+        if word[-1] != word[-2]:
+            new_word += word[-1]
+        if word[0] != word[1]:
+            return word[0] + new_word
+    return new_word
