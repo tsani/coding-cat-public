67eacc5bf271a5c0acc162cf294d14d75f637532
saraisabellabc <sara_isabella@icloud.com>
2024-12-01 14:22:26 -0500

    Create mutation_1.py

---
+++ double-every-nth-char/mutation_1.py
@@ -0,0 +1,12 @@
+def double_nth_char(text: str, n: int) -> str:
+"""
+Bug: Incorrect use of str() function to convert list into string (still returns it as a list instead of a string)
+"""
+    result = []
+    for i, char in enumerate(text, start=1): 
+        if i % n == 0:  
+            result.append(char * 2)  
+        else:
+            result.append(char)
+    return str(result)
+    
