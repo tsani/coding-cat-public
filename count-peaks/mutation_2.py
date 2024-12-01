aa527dcb2a86a0b934275364a9bd83127621e5fa
Tariq-Menla <tariqmenla@gmail.com>
2024-12-01 15:23:58 -0500

    Remutated to the ideal code

--- count-peaks/mutation_2.py
+++ count-peaks/mutation_2.py
@@ -1,14 +1,10 @@
 def count_peaks(nums):
     """
         Used OR instead of AND in the second if statement.
-    """
+    """    
     count = 0
-    for num in enumerate(nums):
-        if num[0] == 0 or num[0] == len(nums) - 1:
-            continue
-        prev = nums[num[0] - 1]
-        next = nums[num[0] + 1]
-
-        if prev < num[1] or num[1] > next: #Wrong operator here
+    for i in range(1, len(nums) - 1):
+        if nums[i] > nums[i - 1] or nums[i] > nums[i + 1]: #Wrong logical operator on this line.
             count += 1
     return count
+
