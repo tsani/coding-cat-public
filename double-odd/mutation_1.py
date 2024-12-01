55b50eb8782c3512ff7e22543d045274c099f291
Sebastian42-5 <sebassotosandrea@gmail.com>
2024-11-30 20:53:32 -0500

    fixing mutation 1 and mutation 3

--- double-odd/mutation_1.py
+++ double-odd/mutation_1.py
@@ -3,7 +3,7 @@ def double_odd(i_list):
     Missing the index skip to properly check the values at odd index 
     ''' 
     new_list = []
-        for i in range(1, len(i_list)):
-            if i_list[i] % 2 != 0:
-                new_list.append(i_list[i])
+    for i in range(1, len(i_list)):
+        if i_list[i] % 2 != 0:
+            new_list.append(i_list[i])
     return new_list
--- double-odd/mutation_3.py
+++ double-odd/mutation_3.py
@@ -1,12 +1,11 @@
 def double_odd(i_list):
     ''''
-    adding a faulty extra if statement to check for the odd index, even though it is already covered ar the range set for the for loop 
+    forgetting to add the index skip for the first index 0 by nit putting the 1 at the beginning of the range 
+
     ''' 
 
     new_list = []
-    for i in range(1, len(i_list), 2):
+    for i in range(len(i_list), 2):
         if i_list[i] % 2 != 0:
-            new_list.append(i_list[i])
-        elif i % 2 != 0:
             new_list.append(i_list[i])
     return new_list
