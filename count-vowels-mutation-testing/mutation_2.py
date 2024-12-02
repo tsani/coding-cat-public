2abea33e9b06a567b30c5f414c4b97ef5e6b34c5
LChan14 <contactliamchan@gmail.com>
2024-12-02 16:38:29 -0500

    Update mutation_2.py

--- count-vowels-mutation-testing/mutation_2.py
+++ count-vowels-mutation-testing/mutation_2.py
@@ -1,6 +1,6 @@
 def count_vowels(string):
     '''
-    Mutation 2: missing case for capital vowels
+    Mutation 2: missing letter
     '''
     vowels = 'aeiou'
     return sum(1 for char in string if char in vowels)
