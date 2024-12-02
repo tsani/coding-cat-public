a44e765f4c2a232f748c048c89ec812804101bd2
Luke A. <129181465+Lolosaurus@users.noreply.github.com>
2024-12-02 16:05:48 -0500

    Update mutation_4.py

--- list_equalizer/mutation_4.py
+++ list_equalizer/mutation_4.py
@@ -1,7 +1,7 @@
 def list_match(list_1, list_2):
-'''
-does not compare at correct value
-'''
+   '''
+      does not compare at correct value
+   '''
    min_length = min(len(list_1), len(list_2))
    count = 0
    for i in range(min_length):
