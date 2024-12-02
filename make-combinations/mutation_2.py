3402875f480d4026b83cbb0b268fc0a4d26555ad
Evan Luo <contact@ohevan.com>
2024-12-01 22:27:58 -0500

    docs: better description

--- make-combinations/mutation_2.py
+++ make-combinations/mutation_2.py
@@ -1,5 +1,5 @@
 def make_combinations(letters, numbers):
     '''
-    Swaps the logic when creating the output pairs, which will result in a different order of the output pairs since the inner loop is now iterating over 'letters' instead of 'numbers'.
+    Iterates over 'numbers' first instead of 'letters'.
     '''
     return [[letter, number] for number in numbers for letter in letters]
