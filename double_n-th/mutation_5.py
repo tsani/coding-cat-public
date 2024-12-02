9a586301e7431bd446a508b635755c8951c534ff
DinnerDinner <147333446+DinnerDinner@users.noreply.github.com>
2024-12-01 23:36:02 -0500

    Update mutation_5.py

--- double_n-th/mutation_5.py
+++ double_n-th/mutation_5.py
@@ -1,14 +1,14 @@
-def double_n_th(string, n):
+def double_nth(str,n):
     """
     Bug: Forgets to join the list of characters into a string at the end.
     """
-    if n <= 0 or not string:
+    if n <= 0 or not str:
         return ""
-    if n > len(string):
-        return string
+    if n > len(str):
+        return str
 
     result = []
-    for i, char in enumerate(string):
+    for i, char in enumerate(str):
         if (i + 1) % n == 0:
             result.append(char * 2)
         else:
