7af2970202d2e5fa210e35bd2b8183effd7a7a72
JM-The-4th <enzo.monroy.iv@gmail.com>
2024-12-02 15:12:03 -0500

    Add files via upload

---
+++ mutation_5.py
@@ -0,0 +1,18 @@
+def fibo_upto(n):
+
+    #Appending wrong element
+
+    fibolist = []
+    a, b = 0, 1
+    if n % 1 == 0 and n >= 0:
+        while a <= n:
+            
+                print(a, end=' ')
+                fibolist.append(b)
+                a, b = b, a + b
+        return fibolist
+    else:
+        print("No")
+        return False
+print(fibo_upto(26))
+      
