908e84983fdaf65952caf6048b2efe47a1cad073
Luke A. <129181465+Lolosaurus@users.noreply.github.com>
2024-12-02 16:05:32 -0500

    indented the message

--- list_equalizer/mutation_3.py
+++ list_equalizer/mutation_3.py
@@ -1,7 +1,7 @@
 def list_match(list_1, list_2):
-'''
-does not check the full list
-'''
+   '''
+      does not check the full list
+   '''
    min_length = min(len(list_1), len(list_2))
    count = 0
    for i in range(min_length-1):
