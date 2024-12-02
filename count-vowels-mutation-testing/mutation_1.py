872d10d494cdbe7afbc0c3804ec88e16acf39e3c
LChan14 <contactliamchan@gmail.com>
2024-12-02 16:37:45 -0500

    Update mutation_1.py

--- count-vowels-mutation-testing/mutation_1.py
+++ count-vowels-mutation-testing/mutation_1.py
@@ -1,7 +1,7 @@
 def count_vowels(string):
     '''
-    Mutation 1:
+    Mutation 1: Wrong condition check
     '''
     vowels = 'aeiouyAEIOUY'
 
-    return sum(1 for char in string if char in vowels)
+    return sum(1 for char in string if char == vowels)
