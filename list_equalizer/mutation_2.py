d996f21e16088f1c20cb65b284562a33dca92f9a
Luke A. <129181465+Lolosaurus@users.noreply.github.com>
2024-12-02 00:25:43 -0500

    Updated mutation_2.py

--- list_equalizer/mutation_2.py
+++ list_equalizer/mutation_2.py
@@ -3,8 +3,9 @@ def list_match(list_1, list_2):
 '''
 count already has a starting value
 '''
+   min_length = min(len(list_1), len(list_2))
    count = 1
-   for i in range(len(list_1)):
+   for i in range(min_length):
       if list_1[i] == list_2[i]:
          count += 1
    return count
