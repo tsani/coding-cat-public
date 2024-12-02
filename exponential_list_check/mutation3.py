b549cd3a419ea413b5e786af98999c1b2512f893
Jutulli <157142993+Jutulli@users.noreply.github.com>
2024-12-01 22:26:09 -0500

    Create mutation3.py

---
+++ exponential_list_check/mutation3.py
@@ -0,0 +1,4 @@
+def exponentialListCheck(nums_list):
+        for i in range(0, len(nums_list) - 1):
+               if (nums_list[i] < nums_list[i + 1]) != False:
+                      return False
