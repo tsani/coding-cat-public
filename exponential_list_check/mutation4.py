9ce103f3280fc86c6a12e9a6f72060fafab55946
Jutulli <157142993+Jutulli@users.noreply.github.com>
2024-12-01 22:29:54 -0500

    Update mutation4.py

--- exponential_list_check/mutation4.py
+++ exponential_list_check/mutation4.py
@@ -1,4 +1,5 @@
 def exponentialListCheck(nums_list):
         for i in range(0, len(nums_list) - 1):
-               if (nums_list[i] < nums_list[i + 1]) != True:
+               if (nums_list[i+1] < nums_list[i]) != True:
                       return False
+        return True
