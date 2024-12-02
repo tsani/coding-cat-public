bcbe43e881470373a0881037d709e90a2043b601
Adam <Adam.Idhoumad@dawsoncollege.qc.ca>
2024-12-01 21:45:50 -0500

    Real Solution

---
+++ remove-vowels-once/solution.py
@@ -0,0 +1,13 @@
+def remove_vowels_once(word: str) -> str:
+    vowels = "aeiouyAEIOUY"
+    seen_vowels = set()
+    result = []
+    for char in word:
+        if char in vowels:
+            if char not in seen_vowels:
+                seen_vowels.add(char)
+            else:
+                result.append(char)
+        else:
+            result.append(char)
+    return ''.join(result)
