67b099af3d74d660bcaac717adb1121ea2493ead
Luke A. <129181465+Lolosaurus@users.noreply.github.com>
2024-12-02 00:16:15 -0500

    Created solution.py

---
+++ list_equalizer/solution.py
@@ -0,0 +1,7 @@
+def list_match(list_1, list_2):
+   min_length = min(len(list_1), len(list_2))
+   count = 0
+   for i in range(min_length):
+      if list_1[i] == list_2[i]:
+         count += 1
+   return count
