97339c957ee70297740225f1c534fd863c9264d1
loicn <l@l>
2024-12-02 00:23:36 -0500

    Solution to partners problem

---
+++ adjacent-vowels/solution.py
@@ -0,0 +1,9 @@
+def adjacent_vowels(word: str) -> int:
+    count = 0
+    vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
+    for index in range(len(word)-1):
+        if word[index] in vowels and word[index + 1] in vowels:
+            count += 1
+
+    return count
+
