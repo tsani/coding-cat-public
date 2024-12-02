82b839b7cfd7337d5935960e0e4b26647cd1c5de
Narshael <Narsh2007@gmail.com>
2024-12-02 14:53:25 -0500

    final adjustements

--- double-letter/mutation_3.py
+++ double-letter/mutation_3.py
@@ -1,16 +1,16 @@
 def double_letter(word):
     '''
-        Change count condition,
-        looking at the wrong consonant
-    '''
+        Change comparison logic, "if result not in consonants"
+        will cause the code to look at non-consonant characters instead
+    ''' 
     consonants = "bcdfghjklpmntrqsvwxyzBCDFGHJKLMNPQRSTVWXYZ"  
     consonant_count = 0 
     result = list(word) 
 
     for i in range(len(result)):
-        if result[i] in consonants:
+        if result[i] not in consonants: #Wrong comparison
             consonant_count += 1
-            if consonant_count == 3: # Mutation looking for the third consonant instead of the second consonant
+            if consonant_count == 2: 
                 result[i] = result[i] * 2 
                 break 
     return "".join(result) 
