7945dd2bbadb2d8b0c2dc87ad0eecc40cffc0326
DinnerDinner <147333446+DinnerDinner@users.noreply.github.com>
2024-12-01 23:34:13 -0500

    Delete count-peaks/solution.py

--- count-peaks/solution.py
+++
@@ -1,8 +0,0 @@
-def count_peaks(nums):
-    peak_count = 0
-    
-    for i in range(1, len(nums) - 1): #from 2nd to before laster element
-        if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
-            peak_count += 1
-    
-    return peak_count
