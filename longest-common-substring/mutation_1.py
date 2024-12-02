ab002265ea514672ebe3b92af65a12b2535d45bb
unknown <nicoleslanina12@gmail.com>
2024-12-02 14:51:40 -0500

    typo2

--- longest-common-substring/mutation_1.py
+++ longest-common-substring/mutation_1.py
@@ -6,7 +6,7 @@ def longest_common_substring(a, b):
     for i in range(len(a)):
         for j in range(len(b)):
             length = 0
-            while i + length < len(a) and j + length < len(b) and a[i + length] == b[j + length]:
+            while i + length < len(a) and j + length < len(b) and a[1 + length] == b[1 + length]:
                 length += 1
             max_len = max(max_len, length)
     return max_len
