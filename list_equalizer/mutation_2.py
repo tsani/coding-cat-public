3dae7dd6d77ed1eac3a606a67c8f805807fd7b19
Luke A. <129181465+Lolosaurus@users.noreply.github.com>
2024-12-02 16:05:15 -0500

    indented the message

--- list_equalizer/mutation_2.py
+++ list_equalizer/mutation_2.py
@@ -1,7 +1,7 @@
 def list_match(list_1, list_2):
-'''
-count already has a starting value
-'''
+   '''
+      count already has a starting value
+   '''
    min_length = min(len(list_1), len(list_2))
    count = 1
    for i in range(min_length):
