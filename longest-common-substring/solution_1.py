1c8f674a0aa80266f3d8d1f59baa4105183e2bc6
unknown <nicoleslanina12@gmail.com>
2024-12-02 15:48:00 -0500

    tweak

--- longest-common-substring/solution_1.py
+++ longest-common-substring/solution_1.py
@@ -1,10 +1,10 @@
 def longest_common_substring(a, b):
     max_len = 0
     for i in range(len(a)):
-        for j in range(i + 1, len(a) + 1):
-            substring = a[i:j]
-            if substring in b:
-                max_len = max(max_len, j - i) 
-                
+        for j in range(len(b)):
+            length = 0
+            while i + length < len(a) and j + length < len(b) and a[i + length] == b[j + length]:
+                length += 1
+            max_len = max(max_len, length)
     return max_len
 
