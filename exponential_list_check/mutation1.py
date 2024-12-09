396738efc3786fd06fa8ded0568c9c2f8d6237d8
Jutulli <157142993+Jutulli@users.noreply.github.com>
2024-12-08 22:05:26 -0500

    Update mutation1.py

--- exponential_list_check/mutation1.py
+++ exponential_list_check/mutation1.py
@@ -1,4 +1,5 @@
 def exponentialListCheck(nums_list):
+        """Writing the wrong equal sign == instead of !="""
         for i in range(0, len(nums_list) - 1):
                if (nums_list[i] < nums_list[i + 1]) == True:
                       return False
