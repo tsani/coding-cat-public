7c3708a046aee6fd86932afb7b8ee8b9df351e1a
Jutulli <157142993+Jutulli@users.noreply.github.com>
2024-12-08 22:07:40 -0500

    Update mutation3.py

--- exponential_list_check/mutation3.py
+++ exponential_list_check/mutation3.py
@@ -1,4 +1,5 @@
 def exponentialListCheck(nums_list):
+        """Wrong condition check (False instead of True)"""
         for i in range(0, len(nums_list) - 1):
                if (nums_list[i] < nums_list[i + 1]) != False:
                       return False
