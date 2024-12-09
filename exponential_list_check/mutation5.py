aa861aa9af1fee155a9e877f50276609f0bc7ffd
Jutulli <157142993+Jutulli@users.noreply.github.com>
2024-12-08 22:09:18 -0500

    Update mutation5.py

--- exponential_list_check/mutation5.py
+++ exponential_list_check/mutation5.py
@@ -1,4 +1,5 @@
 def exponentialListCheck(nums_list):
+        """Forgetting to return True when the list is exponential"""
         for i in range(0, len(nums_list) - 1):
                if (nums_list[i] < nums_list[i + 1]) != True:
                       return False
