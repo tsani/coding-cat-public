6cab87fc391cb5eae48c22c39941a0306a3ac95c
Jo-9m-n1 <leejongmin1092@gmail.com>
2024-12-18 13:06:09 -0500

    Update solution.py
    Added solution.py

--- sum-of-elements/solution.py
+++ sum-of-elements/solution.py
@@ -1,1 +1,11 @@
-
+def unique_sum(lst):
+  unique_elements = set()
+  duplicates = set()
+  
+  for num in lst:
+    if num in unique_elements: """If the number is already unique, it becomes a duplicate"""
+      unique_elements.remove(num)
+      duplicates.add(num)
+    elif num not in duplicates: """If it's not a duplicate and not yet unique, add it"""
+      unique_elements.add(num)
+  return sum(unique_elements) """Sum all elements that are unique"""
