85ed8c59ec2fe7a33be98612fe44d29e05b033d3
Jutulli <157142993+Jutulli@users.noreply.github.com>
2024-12-18 10:57:38 -0500

    Update and rename Solution.py to solution.py

--- exponential_list_check/solution.py
+++ exponential_list_check/solution.py
@@ -1,4 +1,4 @@
-def exponentialListCheck(nums_list):
+def exponential_list_check(nums_list):
         """Solution Code"""
         for i in range(0, len(nums_list) - 1):
                if (nums_list[i] < nums_list[i + 1]) != True:
