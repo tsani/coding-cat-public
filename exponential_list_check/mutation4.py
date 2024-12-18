7b285a57622d7f77ff1d9be5b1c53e04d3dcc0ca
Jutulli <157142993+Jutulli@users.noreply.github.com>
2024-12-18 10:58:47 -0500

    Update mutation4.py

--- exponential_list_check/mutation4.py
+++ exponential_list_check/mutation4.py
@@ -1,4 +1,4 @@
-def exponentialListCheck(nums_list):
+def exponential_list_check(nums_list):
         """Inversing the number checks in the comparaison (switching [i+1] and [i]"""
         for i in range(0, len(nums_list) - 1):
                if (nums_list[i+1] < nums_list[i]) != True:
